3f9e63939efc5ac870e8dbc6ba61455be0c7239a
Igor <Igor@example.com>
2019-03-18 20:39:40 +0300

    added app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Calculator from './applications/Tarasov_calc';
 
 import Clicker from './applications/clicker';
 import AppsTable from './AppsTable';
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="container">
           <Route path="/" exact component={AppsTable} />
           <Route path="/app1" exact component={Clicker}/>
+          <Route path="/Igor_Tarasov" exact component={Calculator}/>
         </div>
       </Router>
     );
--- src/AppsTable.js
+++ src/AppsTable.js
@@ -22,6 +22,12 @@ function AppsTable() {
           <td>Евгений</td>
           <td><Link className="btn btn-success" to="/app1">Перейти {'>'}</Link></td>
         </tr>
+        <tr>
+          <th scope="row">1</th>
+          <td>Калькулятор</td>
+          <td>Тарасов</td>
+          <td><Link className="btn btn-success" to="/Igor_Tarasov">Перейти {'>'}</Link></td>
+        </tr>
         </tbody>
       </table>
     </div>
