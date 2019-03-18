2dc9538082e9342dfebef33dc291ee53404b75ad
Kostya <4epubelik@gmail.com>
2019-03-18 21:31:57 +0300

    ss

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Clicker from './applications/clicker';
+import SpaceInfo from './applications/SonaTa_spaceinfo';
 import AppsTable from './AppsTable';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <Route path="/" exact component={AppsTable} />
-          <Route path="/app1" exact component={Clicker}/>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <Route path="/" exact component={AppsTable} />
+                    <Route path="/app1" exact component={Clicker}/>
+                    <Route path="/SonaTa" exact component={SpaceInfo}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/applications/SonaTa_spaceinfo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import withHeader from './../../hocs/withHeader';
+
+function SpaceInfo() {
+    return (
+        <div>Space</div>
+    );
+}
+
+export default withHeader(SpaceInfo);
