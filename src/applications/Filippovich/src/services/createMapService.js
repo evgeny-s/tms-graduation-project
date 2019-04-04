dc67fdbe5ffbff1d3f09b4d9cf2890981a65a3c9
pyfkin <pyfkin@gmail.com>
2019-04-05 00:44:10 +0300

    add random map medium

--- src/applications/Filippovich/src/services/createMapService.js
+++ src/applications/Filippovich/src/services/createMapService.js
@@ -18,15 +18,20 @@ class createMapService
     {
         let map = [];
         map = this._createGrid(level, map);
-
         map = this._createWalls(level, map);
+        map = this._createSkills(level, map);
+        map = this._createCertification(level, map);
+        map = this._createUltimate(level, map);
+        map = this._createMedecine(level, map);
+        map = this._createBoss(level, map);
+
 
         return map;
     }
 
     _getRandomInt(min, max)
     {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     _createGrid(level, map)
@@ -39,7 +44,7 @@ class createMapService
                         map[i].push(itemTypes.POLE);
                     }
                 }
-                return map;
+                break;
             case 2:
                 for (let i = 0; i < 35; i++) {
                     map.push([]);
@@ -47,93 +52,208 @@ class createMapService
                         map[i].push(itemTypes.POLE);
                     }
                 }
-                return map;
+                break;
             case 3:
-                for (let i = 0; i < 45; i++) {
+                for (let i = 0; i < 51; i++) {
                     map.push([]);
                     for (let j = 0; j < 20; j++) {
                         map[i].push(itemTypes.POLE);
                     }
                 }
-                return map;
+                break;
             default:
-                return [
-                    [
-                        "player",
-                        "player",
-                        "player",
-                        "player",
-                        "player"
-                    ],
-                    [
-                        "player",
-                        "player",
-                        "player",
-                        "player",
-                        "player"
-                    ]
-                ];
+                break;
         }
+        return map;
     }
 
     _createWalls(level, map)
     {
-        let wallsCount, wallPerLineCount;
-
         switch (level) {
             case 1:
-                wallsCount = this._getRandomInt(90, 110);
-                wallPerLineCount = Math.floor(wallsCount / 25);
-                console.log(wallPerLineCount);
                 for (let i = 0; i < 25; i++) {
-                    for (let j = 0; j < 20; j++) {
-                        if (wallPerLineCount !== 0) {
-                            if (this._getRandomInt(0, 1)) {
-                                map[i][j] = itemTypes.WALL;
-                                wallPerLineCount--;
-                            }
-                        }
-
+                    let indexOfWall = [];
+                    let wallPerLineCount = Math.floor(this._getRandomInt(3, 5));
+                    for (let k = 0; k < wallPerLineCount; k++) {
+                        indexOfWall.push(this._getRandomInt(0, 19))
                     }
-                    wallPerLineCount =  Math.floor(wallsCount / 25);
+                    for (let j = 0; j < indexOfWall.length; j++) {
+                        map[i][indexOfWall[j]] = itemTypes.WALL;
+                    }
                 }
-                return map;
+                break;
             case 2:
                 for (let i = 0; i < 35; i++) {
-                    map.push([]);
-                    for (let j = 0; j < 20; j++) {
-                        map[i].push(itemTypes.POLE);
+                    let indexOfWall = [];
+                    let wallPerLineCount = Math.floor(this._getRandomInt(4, 6));
+                    for (let k = 0; k < wallPerLineCount; k++) {
+                        indexOfWall.push(this._getRandomInt(0, 19))
+                    }
+                    for (let j = 0; j < indexOfWall.length; j++) {
+                        map[i][indexOfWall[j]] = itemTypes.WALL;
                     }
                 }
-                return map;
+                break;
             case 3:
-                for (let i = 0; i < 45; i++) {
-                    map.push([]);
-                    for (let j = 0; j < 20; j++) {
-                        map[i].push(itemTypes.POLE);
+                for (let i = 0; i < 51; i++) {
+                    let indexOfWall = [];
+                    let wallPerLineCount = Math.floor(this._getRandomInt(5, 7));
+                    for (let k = 0; k < wallPerLineCount; k++) {
+                        indexOfWall.push(this._getRandomInt(0, 19))
+                    }
+                    for (let j = 0; j < indexOfWall.length; j++) {
+                        map[i][indexOfWall[j]] = itemTypes.WALL;
                     }
                 }
-                return map;
+                break;
             default:
-                return [
-                    [
-                        "player",
-                        "player",
-                        "player",
-                        "player",
-                        "player"
-                    ],
-                    [
-                        "player",
-                        "player",
-                        "player",
-                        "player",
-                        "player"
-                    ]
-                ];
+                break;
         }
+        return map;
     }
 
+    _createSkills(level, map)
+    {
+        switch (level) {
+            case 1:
+                for (let i = 0; i < 25; i++) {
+                    let skillIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][skillIndex] = itemTypes.SKILL;
+                }
+                break;
+            case 2:
+                for (let i = 0; i < 35; i++) {
+                    if (!(i%5)) continue;
+                    let skillIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][skillIndex] = itemTypes.SKILL;
+                }
+                break;
+            case 3:
+                for (let i = 0; i < 51; i+=2) {
+                    let skillIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][skillIndex] = itemTypes.SKILL;
+                }
+                break;
+            default:
+                break;
+
+        }
+        return map;
+    }
+
+    _createCertification(level, map)
+    {
+        switch (level) {
+            case 1:
+                for (let i = 0; i < 25; i++) {
+                    if ( i%4 || i === 0 ) continue;
+                    let certificationIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][certificationIndex] = itemTypes.CERTIFICATION;
+                }
+                break;
+            case 2:
+                for (let i = 0; i < 35; i++) {
+                    if (i%7 || i === 0) continue;
+                    let certificationIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][certificationIndex] = itemTypes.CERTIFICATION;
+                }
+                break;
+            case 3:
+                for (let i = 0; i < 51; i++) {
+                    if ( i%7 || i === 0 ) continue;
+                    let certificationIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][certificationIndex] = itemTypes.CERTIFICATION;
+                }
+                break;
+            default:
+                break;
+
+        }
+        return map;
+    }
+
+    _createUltimate(level, map)
+    {
+        switch (level) {
+            case 1:
+                for (let i = 0; i < 25; i++) {
+                    if ( i%7 || i === 0 ) continue;
+                    let ultimateIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][ultimateIndex] = itemTypes.ULTIMATE;
+                }
+                break;
+            case 2:
+                for (let i = 0; i < 35; i++) {
+                    if ( i%8 || i === 0 ) continue;
+                    let ultimateIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][ultimateIndex] = itemTypes.ULTIMATE;
+                }
+                break;
+            case 3:
+                for (let i = 0; i < 51; i++) {
+                    if ( i%11 || i === 0 ) continue;
+                    let ultimateIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][ultimateIndex] = itemTypes.ULTIMATE;
+                }
+                break;
+            default:
+                break;
+
+        }
+        return map;
+    }
+
+    _createMedecine(level, map)
+    {
+        switch (level) {
+            case 1:
+                for (let i = 0; i < 25; i++) {
+                    if ( i%8 || i === 0 ) continue;
+                    let medecineIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][medecineIndex] = itemTypes.MEDECINE;
+                }
+                break;
+            case 2:
+                for (let i = 0; i < 35; i++) {
+                    if ( i%11 || i === 0 ) continue;
+                    let medecineIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][medecineIndex] = itemTypes.MEDECINE;
+                }
+                break;
+            case 3:
+                for (let i = 0; i < 51; i++) {
+                    if ( i%17 || i === 0 ) continue;
+                    let medecineIndex = Math.floor(this._getRandomInt(0, 19));
+                    map[i][medecineIndex] = itemTypes.MEDECINE;
+                }
+                break;
+            default:
+                break;
+
+        }
+        return map;
+    }
+
+    _createBoss(level,map)
+    {
+        switch (level) {
+            case 1:
+                let bossIndex = Math.floor(this._getRandomInt(0, 24));
+                map[bossIndex][5] = itemTypes.BOSS;
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            default:
+                break;
+
+        }
+        return map;
+    }
 }
+
 
 export default new createMapService();
