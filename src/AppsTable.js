ea13bfc05ddc1c00084839bed36c545a742d7649
pyfkin <pyfkin@gmail.com>
2019-04-09 14:05:52 +0300

    table numeration changed

--- src/AppsTable.js
+++ src/AppsTable.js
@@ -23,13 +23,13 @@ function AppsTable() {
                     <td><Link className="btn btn-success" to="/app1">Перейти {'>'}</Link></td>
                 </tr>
                 <tr>
-                    <th scope="row">1</th>
+                    <th scope="row">2</th>
                     <td>Игра</td>
                     <td>Сергей</td>
                     <td><Link className="btn btn-success" to="/s-sushkevich">Перейти {'>'}</Link></td>
                 </tr>
                 <tr>
-                    <th scope="row">2</th>
+                    <th scope="row">3</th>
                     <td>Бродилка</td>
                     <td>Кирилл</td>
                     <td><Link className="btn btn-success" to="/filippovich">Перейти {'>'}</Link></td>
