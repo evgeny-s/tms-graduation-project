aeec5023e610af1f1f4fb7a07ee85c35ac313e60
Sergey Sushkevich <s.sushkevich92@gmail.com>
2019-04-11 16:30:49 +0300

    make eject

---
+++ src/applications/sushkevich/components/AudioPlayer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AudioPlayer = (props) => {
+    return (
+        <audio>
+            <source
+                type="audio/mp3"
+                src="https://t4.bcbits.com/stream/6484b6d47aaf225751917803323abcba/mp3-128/375292196?p=0&ts=1555075705&t=930b3cd8d69bd6da4367c3e04dd6865194709d60&token=1555075705_e752908ced6fa445183f1a3d72764387c049971f"/>
+        </audio>
+    )
+};
+
+export default AudioPlayer;
