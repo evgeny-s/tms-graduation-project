9981fd04d46fe4f0ef7e19a72a20bb7f289e5fec
SonaSun <sona4ka_90@mail.ru>
2019-04-14 22:42:12 +0300

    Astronauts and Time changes

--- src/applications/SonaTa_spaceinfo/components/App.js
+++ src/applications/SonaTa_spaceinfo/components/App.js
@@ -6,48 +6,50 @@ import {AstronautsCount, Astronauts} from './Astronauts.js';
 import space from './space.jpg';
 
 
-
 class App extends React.Component {
-    constructor(props){
-        super(props)
-        this.time=this.time.bind(this);
- 
-    }
-
+  
     componentWillMount() {
         setInterval(() => {
             this.props.resolvedGetData()
-        }, 5000);
+        }, 1000);
         
-      this.props.getAstronauts()
+      this.props.getAstronauts();
          
     }
-   time(){
-        return Math.floor(this.props.timestamp % 86400 / 3600) + ':' + Math.floor(this.props.timestamp % 86400 % 3600 / 60) + ':' + Math.floor(this.props.timestamp % 86400 % 3600 % 60);
+   
+    time(){
+	   const unix = this.props.timestamp;
+	   const hours = Math.floor(unix % 86400 / 3600).toString();
+	   const min = Math.floor(unix % 86400 % 3600 / 60).toString();
+	   const sec = Math.floor(unix % 86400 % 3600 % 60).toString();
+	   const h = hours.length < 2 ? 0 + hours : hours;
+	   const m = min.length < 2 ? 0 + min : min;
+	   const s = sec.length < 2 ? 0 + sec : sec;
+
+	   return h + ':' + m + ':' + s;
     }
+    
+   people(){ if(this.props.people){
+       return this.props.people.map((per)=>{return <li key={per.toString()}>{per}</li>})}
+   }
     
     render(){
         const page={ backgroundImage: "url(" + space + ")", backgroundPosition: "center", backgroundSize: "cover", width:"1110px", height:"550px",color:"yellow"}
-       
+        
         return (
             <div style={page}>
-                
-                <AstronautsCount value = {this.props.numberOfPeople} />
-            
-                {/* <Astronauts value = {console.log(this.props.people)} />*/}
-          
-               {/* <ul>
-                    {this.props.people.map(per=><Astronauts key={per.toString()} value = {per} />)} 
-                </ul>*/}
-
-                          
+                <div style={{float:"right"}}>
+                    <AstronautsCount value = {this.props.numberOfPeople} />
+                    <Astronauts person = {this.people()} />
+                </div>
+                        
                 <p style={{color:"yellow", fontSize:"25px", margin:"20px"}}>Current Data on the ISS:</p>
                 <LatLong text = "Current Latitude:" value={this.props.latitude} />
                 <LatLong text = "Current Longitude:" value={this.props.longitude}/>
                 <Timestamp text = "Current Timestamp(unix):" value={this.props.timestamp}/>
-                <Timestamp text = "Current Timestamp(natural):" value={this.time}/>
+                <Timestamp text = "Current Timestamp(natural):" value={this.time()}/>
             
-                <Velocity />
+                {/*<Velocity />*/}
             </div>
         )
       
--- src/applications/SonaTa_spaceinfo/components/Astronauts.js
+++ src/applications/SonaTa_spaceinfo/components/Astronauts.js
@@ -2,17 +2,15 @@ import React from 'react';
 
 function AstronautsCount(props){
      return (
-        <div style={{color:"#75aaf9", fontSize:"25px", margin:"10px",float:"right"}}>At this moment there are {props.value} humans in space. They are:</div>
+        <div style={{color:"#75aaf9", fontSize:"25px", margin:"10px", textAlign:"center"}}>At this moment there are {props.value} humans in space. <br/>They are:</div>
          )
     }
 
 
 function Astronauts(props){
-   
      return (
-            <li style={{color:"#6492d6", fontSize:"15px", margin:"10px", float:"right"}}>{props.value}</li>
-        );
-}
-
+        <ul style={{color:"#6492d6", fontSize:"15px", marginLeft:"250px"}}>{props.person}</ul>
+        )
+   }
 
 export {AstronautsCount, Astronauts};
